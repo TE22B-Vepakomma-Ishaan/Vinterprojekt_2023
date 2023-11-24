06c3465fdc1e5c3effc59f08a9095388bfbc4f54
Ishaan Vepakomma <ishaan.vepakomma@elev.ga.ntig.se>
2023-11-24 18:32:59 +0100

    added main game screen

--- Vinterprojekt_2023/Program.cs
+++ Vinterprojekt_2023/Program.cs
@@ -5,12 +5,48 @@ using Raylib_cs;
 Raylib.InitWindow(1300, 1000, "Ghost");
 Raylib.SetTargetFPS(60);
 
+
+string screen = "menu";
+
+
+
+
+
+
+
+
+
 while (!Raylib.WindowShouldClose())
 { 
-    Raylib.BeginDrawing();
+    
+   Raylib.BeginDrawing();
 
+
+
+
+
+ 
+   if(screen == "menu")
+    {
+
+        Raylib.ClearBackground(Color.BLACK);
+        Raylib.DrawText("press SPACE to start", 450, 500, 35, Color.WHITE);
+
+        if(Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
+        {
+           screen = "game"; 
+        }
+    } 
+
+    if (screen == "game"){
+    
     Raylib.ClearBackground(Color.BLACK);
-    Raylib.DrawText("press SPACE to start", 450, 500, 35, Color.WHITE);
+    Raylib.DrawRectangleLines(20, 20, 800, 800,Color.DARKPURPLE);
+
+    }
+
+    
+
 
 
 
