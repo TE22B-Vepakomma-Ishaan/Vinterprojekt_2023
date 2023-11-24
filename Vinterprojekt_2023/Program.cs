bc6c42b20681cab2b3e1a2f7899d7f4d1aa58460
Ishaan Vepakomma <ishaan.vepakomma@elev.ga.ntig.se>
2023-11-24 18:25:47 +0100

    first commit- main menu added

---
+++ Vinterprojekt_2023/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+using Raylib_cs;
+
+Raylib.InitWindow(1300, 1000, "Ghost");
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{ 
+    Raylib.BeginDrawing();
+
+    Raylib.ClearBackground(Color.BLACK);
+    Raylib.DrawText("press SPACE to start", 450, 500, 35, Color.WHITE);
+
+
+
+
+
+
+    Raylib.EndDrawing();  
+    
+ }
+
+   
