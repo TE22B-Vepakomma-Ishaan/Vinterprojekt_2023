b1395cfdff69c0f4f1cbe1633443f9f8b76534d9
Ishaan Vepakomma <ishaan.vepakomma@elev.ga.ntig.se>
2023-11-27 09:29:58 +0100

    fixed sprite to box

--- Vinterprojekt_2023/Program.cs
+++ Vinterprojekt_2023/Program.cs
@@ -12,9 +12,12 @@ string screen = "menu";
 
 Rectangle gameBorder = new(25, 25, 800, 800);
 
+Vector2 border = new(gameBorder.x, gameBorder.y);
+Rectangle character = new(900, 400, 40, 40);
+Texture2D PlayerSprite = Raylib.LoadTexture("GreenBall.png");
 
-
-
+Vector2 movement = new(0,0);
+float speed = 3;
 
 
 
@@ -23,8 +26,35 @@ while (!Raylib.WindowShouldClose())
     
    Raylib.BeginDrawing();
 
+   // Character---------------------------------------------------------------------------------------------------------------
+   
+           movement = Vector2.Zero;
 
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+        {
+            movement.X +=2;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+        {
+            movement.X -=2;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
+        {
+            movement.Y -=2;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
+        {
+            movement.Y +=2;
+        }
 
+        if (movement.Length() > 0)
+        {
+            movement = Vector2.Normalize(movement) * speed;
+        }
+
+        character.x += movement.X;
+        character.y += movement.Y;
+   
 
 
  
@@ -43,7 +73,15 @@ while (!Raylib.WindowShouldClose())
     if (screen == "game"){
     
     Raylib.ClearBackground(Color.BLACK);
-    Raylib.DrawRectangleLinesEx(gameBorder, 5, Color.DARKPURPLE);
+    Raylib.DrawRectangleLinesEx(gameBorder, 5, Color.PURPLE);
+    
+    
+    
+    
+    Raylib.DrawRectangleRec(character, Color.BLANK);
+    Raylib.DrawTexture(PlayerSprite, (int)character.x, (int)character.y, Color.PURPLE);
+
+      
 
     }
 
